d64511738e3c14582d9135d92284bffdd017fc24
bellacooper <coopisa@uw.edu>
2022-01-11 17:24:57 -0800

    Initial Commit

---
+++ reservation-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  reservation-app
+//
+//  Created by Bella Cooper on 1/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ reservation-app/reservation_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  reservation_appApp.swift
+//  reservation-app
+//
+//  Created by Bella Cooper on 1/11/22.
+//
+
+import SwiftUI
+
+@main
+struct reservation_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
